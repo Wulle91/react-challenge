c0cadcb4147be39cbae446acad9a04a281253d06
Wulle91 <mvuckovi91@gmail.com>
2023-05-27 20:18:48 +0000

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <NavBarForm />
+      <NavBarSimple />
+      <Sidebar />
     </div>
   );
 }
---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NavBarForm extends Component {
+  render() {
+    return (
+      <div>NavBarForm</div>
+    )
+  }
+}
+
+export default NavBarForm
